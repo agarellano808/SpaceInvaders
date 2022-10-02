e530e24087b13ad183a6c4cc05b9177e8b5dcbe7
Double A <agarellano353@gmail.com>
2022-10-01 19:52:19 -0500

    buttonPanel is now its own class

---
+++ src/ButtonPanel.java
@@ -0,0 +1,61 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class ButtonPanel extends JPanel {
+	private JButton playButton;
+	private JButton highScoreButton;
+	private JButton quitButton;
+	
+	public ButtonPanel() {
+		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		
+		playButton = new JButton("PLAY");
+		playButton.setAlignmentX(CENTER_ALIGNMENT);
+
+		
+		highScoreButton = new JButton("HIGH SCORE");
+		highScoreButton.setAlignmentX(CENTER_ALIGNMENT);
+
+		
+		quitButton = new JButton("QUIT");
+		quitButton.setAlignmentX(CENTER_ALIGNMENT);
+
+	    add(playButton);
+		add(Box.createRigidArea(new Dimension(0,10)));
+		add(highScoreButton);
+		add(Box.createRigidArea(new Dimension(0,10)));
+		add(quitButton);
+	}
+	
+	public void init() {
+		Frame jf= (Frame) SwingUtilities.getWindowAncestor(this);
+		playButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				jf.setVgap(0);
+				jf.setHgap(0);
+				jf.setSize(640,480);
+				jf.changePanel(""+2);
+				
+			}
+		});
+		highScoreButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				jf.changePanel(""+3);
+			}
+		});
+		
+		quitButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				jf.dispose();
+				System.exit(0);
+			}
+		});
+	}
+}
--- src/Frame.java
+++ src/Frame.java
@@ -1,70 +1,39 @@
-import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class Frame extends JFrame{
-	private JPanel buttonPanel;
+public class Frame extends JFrame {
+	private ButtonPanel buttonPanel;
 	private CardLayout cl;
+	private HighScoreScreen s;
+	private Board board;
 	public Frame() {
-	//	add(new Board());
-		
+
 		setTitle("Space Invaders");
-		cl=new CardLayout(100,160);
+		cl = new CardLayout(100, 160);
 		setLayout(cl);
-		setSize(640,480);
+		setSize(640, 480);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		buttonPanel = new JPanel();
-		buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.Y_AXIS));
-	
-		JButton playButton = new JButton("PLAY");
-		playButton.setAlignmentX(CENTER_ALIGNMENT);
-		playButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				cl.setVgap(0);
-				cl.setHgap(0);
-				setSize(640,480);
-				cl.show(getContentPane(),""+2);
-				
-			}
-		});
-		
-		JButton highScoreButton = new JButton("HIGH SCORE");
-		highScoreButton.setAlignmentX(CENTER_ALIGNMENT);
-		highScoreButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				cl.show(getContentPane(),""+3);
-			}
-		});
-		
-		JButton quitButton = new JButton("QUIT");
-		quitButton.setAlignmentX(CENTER_ALIGNMENT);
-		quitButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				System.exit(0);
-			}
-		});
-		buttonPanel.add(playButton);
-		buttonPanel.add(Box.createRigidArea(new Dimension(0,10)));
-		buttonPanel.add(highScoreButton);
-		buttonPanel.add(Box.createRigidArea(new Dimension(0,10)));
-		buttonPanel.add(quitButton);
-		getContentPane().add(buttonPanel,"1");
-		getContentPane().add(new Board(),"2");
-		HighScoreScreen s= new HighScoreScreen();
-		getContentPane().add(s,"3");
+		buttonPanel = new ButtonPanel();
+		getContentPane().add(buttonPanel, "1");
+		buttonPanel.init();
+		board=new Board();
+		getContentPane().add(board, "2");
+	    s = new HighScoreScreen();
+		getContentPane().add(s, "3");
 		s.init();
 		pack();
 	}
 
+	public void changePanel(String s) {
+		cl.show(getContentPane(), s);
+	}
+
+	public void setVgap(int i) {
+		cl.setVgap(0);
+	}
+
+	public void setHgap(int i) {
+		cl.setHgap(0);
+	}
 }
