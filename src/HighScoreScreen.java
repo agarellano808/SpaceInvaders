575bd393d942c5207338504725e5a4dc492ce2e0
Double A <agarellano353@gmail.com>
2022-10-02 19:21:06 -0500

    now displays text and back button now works

--- src/HighScoreScreen.java
+++ src/HighScoreScreen.java
@@ -1,28 +1,66 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class HighScoreScreen extends JPanel {
 	private JButton J;
-	public HighScoreScreen() {
+	private String playerInitials[];
+	private String highScores[];
 
-	}
-	public void init() {
-		JFrame Jf= (JFrame) SwingUtilities.getWindowAncestor(this);
-		
-		J=new JButton("Back");
+	public HighScoreScreen() {
+		setLayout(new BorderLayout());
+		J = new JButton("Back");
 		J.setAlignmentX(CENTER_ALIGNMENT);
-		J.addActionListener( new ActionListener() {
+		add(J, BorderLayout.SOUTH);
+	}
+
+	public void init() {
+		Frame Jf = (Frame) SwingUtilities.getWindowAncestor(this);
+		Jf.setHgap(0);
+		Jf.setVgap(0);
+		Jf.setSize(640, 480);
+		
+		J.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Jf.setBackground(Color.black);
-				Jf.setTitle("Space");
+
+				Jf.changePanel("1");
+				Jf.setHgap(100);
+				Jf.setVgap(160);
+				Jf.pack();
 			}
 		});
-		add(J);
 	}
+
+	public void initalizeHighScores() {
+		playerInitials = new String[10];
+		highScores = new String[10];
+		for (int i = 0; i < 10; i++) {
+			playerInitials[i] = "AAA";
+			highScores[i] = "00000000";
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
+		setBackground(Color.black);
+	}
+
+	public void draw(Graphics g) {
+		for (int i = 0; i < 10; i++) {
+			g.setColor(Color.white);
+			initalizeHighScores();
+			g.drawString(playerInitials[i], 280, 50 + (30 * (i + 1)));
+			g.drawString(highScores[i], 330, 50 + (30 * (i + 1)));
+
+		}
+	}
+
 }
