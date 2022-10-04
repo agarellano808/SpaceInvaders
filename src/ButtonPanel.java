91e2747dfd2f75d598a05c41beae012c4a388db0
Double A <agarellano353@gmail.com>
2022-10-03 20:25:40 -0500

    Refactor button initalizing, adding action Listners are done by seprate methods. Added AncestorListener

--- src/ButtonPanel.java
+++ src/ButtonPanel.java
@@ -1,63 +1,97 @@
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 
 public class ButtonPanel extends JPanel {
 	private JButton playButton;
 	private JButton highScoreButton;
 	private JButton quitButton;
-	
+	private Frame frame;
+	private AncestorListener ancestorListener;
+
 	public ButtonPanel() {
-		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		addButtons();
+		addButtonActionListeners();
+		setAncestorListener();
+	}
+
+	private void addButtons() {
 		playButton = new JButton("PLAY");
 		playButton.setAlignmentX(CENTER_ALIGNMENT);
 
-		
 		highScoreButton = new JButton("HIGH SCORE");
 		highScoreButton.setAlignmentX(CENTER_ALIGNMENT);
 
-		
 		quitButton = new JButton("QUIT");
 		quitButton.setAlignmentX(CENTER_ALIGNMENT);
 
-	    add(playButton);
-		add(Box.createRigidArea(new Dimension(0,10)));
+		add(playButton);
+		add(Box.createRigidArea(new Dimension(0, 10)));
 		add(highScoreButton);
-		add(Box.createRigidArea(new Dimension(0,10)));
+		add(Box.createRigidArea(new Dimension(0, 10)));
 		add(quitButton);
 	}
-	
-	public void init() {
-		Frame jf= (Frame) SwingUtilities.getWindowAncestor(this);
-		playButton.addActionListener(new ActionListener(){
+
+	private void addButtonActionListeners() {
+		playButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				jf.setVgap(0);
-				jf.setHgap(0);
-				jf.setSize(640,480);
-				jf.changePanel(""+2);
-				jf.setFocus();
-				
+				frame.setVgap(0);
+				frame.setHgap(0);
+				frame.setSize(640, 480);
+				frame.changePanel("" + 2);
+				frame.setFocus();
+
 			}
 		});
-		highScoreButton.addActionListener(new ActionListener(){
+
+		highScoreButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				jf.changePanel(""+3);
-				jf.initializeHighscorescreen();
+				frame.changePanel("" + 3);
+				frame.initializeHighscorescreen();
 			}
 		});
-		
-		quitButton.addActionListener(new ActionListener(){
+
+		quitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				jf.dispose();
+				frame.dispose();
 				System.exit(0);
 			}
 		});
+	}
+
+	private void setAncestor() {
+		frame = (Frame) SwingUtilities.getWindowAncestor(this);
+
+	}
+
+	private void setAncestorListener() {
+		ancestorListener = new AncestorListener() {
+
+			@Override
+
+			public void ancestorAdded(AncestorEvent ancestorEvent) {
+				setAncestor();
+			}
+
+			@Override
+			// This method is not being used and has been left intentionally blank
+			public void ancestorMoved(AncestorEvent ancestorEvent) {
+			}
+
+			@Override
+			// This method is not being used and has been left intentionally blank
+			public void ancestorRemoved(AncestorEvent ancestorEvent) {
+			}
+
+		};
+		addAncestorListener(ancestorListener);
 	}
 }
